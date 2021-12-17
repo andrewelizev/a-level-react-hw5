f4b51d05f45286e6e80c9b1e2467ac8165f9c225
Andrew Elizev PC <aelizev@gmail.com>
2021-12-18 01:05:27 +0200

    begin add redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store'
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
@@ -10,16 +12,18 @@ import TableToDos from './components/ToDo/TableToDos/TableToDos'
 
 ReactDOM.render(
 	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<App />}>
-				<Route path="films" element={<Films />} />
-				<Route path="todo" element={<ToDo />} >
-					<Route path='home' element={<TableToDos task='Byu socks' time='on week' />} />
-					<Route path='work' element={<TableToDos task='Make presentation' time='for an hour' />} />
-					<Route path='need' element={<TableToDos task='Make a plan' time='28.12.2021' />} />
+		<Provider store={store}>
+			<Routes>
+				<Route path="/" element={<App />}>
+					<Route path="films" element={<Films />} />
+					<Route path="todo" element={<ToDo />} >
+						<Route path='home' element={<TableToDos task='Byu socks' time='on week' />} />
+						<Route path='work' element={<TableToDos task='Make presentation' time='for an hour' />} />
+						<Route path='need' element={<TableToDos task='Make a plan' time='28.12.2021' />} />
+					</Route>
 				</Route>
-			</Route>
-		</Routes>
+			</Routes>
+		</Provider>
 	</BrowserRouter>,
 	document.getElementById('root')
 );
